90fb0adf26b4a50da73f597122695272232bc520
Your Name <you@example.com>
2024-10-09 10:43:47 +0900

    devlop of dictionary

--- playground/env.c
+++ playground/env.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct s_env
 {
+	char *key;
 	char *value;
 	struct s_env *next;
 }	t_env;
 
+int	count_env(char **envp)
+{
+	int	count = 0;
+	while (envp[count])
+		count++;
+	return (count);
+}
+
 int main(int argc, char **argv, char **envp) {
-	
+	t_env *env_list;
+	int env_len = count_env(envp);
+	env_list = malloc(sizeof(t_env) * env_len);		
+
+	int	i = 0;
+	while (envp[i])
+	{
+		env_list->key = envp
+	}
 }
