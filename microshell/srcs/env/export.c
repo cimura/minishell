2f00653336739f5c83611e775256535007347651
Takato Takino <ttakino@c2r8s4.42tokyo.jp>
2024-10-20 17:56:05 +0900

    envなどのコマンドを確かめやすいようにbuiltin-shellを作って試せるようにした。

--- microshell/srcs/env/export.c
+++ microshell/srcs/env/export.c
@@ -6,11 +6,11 @@
 /*   By: ttakino <ttakino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:00:50 by ttakino           #+#    #+#             */
-/*   Updated: 2024/10/18 18:30:22 by ttakino          ###   ########.fr       */
+/*   Updated: 2024/10/20 17:51:37 by ttakino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "env.h"
+#include "builtin.h"
 
 // 1 -> 見つからない 0 -> 見つかる
 int    ft_strstr(char *haystack, char *needle)
@@ -29,65 +29,109 @@ int    ft_strstr(char *haystack, char *needle)
     return (1);
 }
 
-int	export(char *arg, t_env *env_lst)
+// key部分にspecial_charの1文字が含まれるかどうかを検索する
+bool	is_special_char_in_key(char *arg)
 {
-	t_env	*new;
 	char	*special_char;
-	int		klen;
-	int		vlen;
+	int		i;
 
 	// TODO
 	special_char = "|&;()<> \t\n*";
-	// 引数が key=value の形になっていなかったら正常終了にした（仮）
-	if (!ft_strchr(arg, '='))
-		return (1);
-	// if (ft_strstr(arg, special_char) == 0)
-	// 	return (1);
+	i = 0;
+	while (arg[i] != '\0' && arg[i] != '=')
+	{
+		if (ft_strchr(special_char, arg[i]) != NULL)
+			return (true);
+		i++;
+	}
+	return (false);
+}
 
-	// あとは新しいノード作ってenv_lstに追加してるだけ
+// 与えられたarg(key)があった場合に、そのノードを返す関数
+t_env	*get_node_having_same_key(char *arg, t_env *env_lst)
+{
+	while (env_lst != NULL)
+	{
+		if (ft_strncmp(arg, env_lst->key, ft_strlen(env_lst->key)) == 0)
+			return (env_lst);
+		env_lst = env_lst->next;
+	}
+	return (NULL);
+}
+
+t_env	*set_key_value_registered(t_env *new, char *arg)
+{
+	int	klen;
+	int	vlen;
+
 	vlen = ft_strlen(ft_strchr(arg, '=') + 1);
 	klen = ft_strlen(arg) - vlen - 1;
-
-	
-	new = malloc(sizeof(t_env));
-	if (!new)
-		return (1);
 	new->key = malloc(klen + 1);
 	if (!new->key)
-		return (free(new), new = NULL, 1);
+		return (free(new), new = NULL, NULL);
 	new->value = malloc(vlen + 1);
 	if (!new->value)
-		return (free(new), new = NULL, free(new->key), new->key = NULL, 1);
+		return (free(new), new = NULL, free(new->key), new->key = NULL, NULL);
 	new->key = ft_memmove(new->key, arg, klen);
 	new->key[klen] = '\0';
-	if (ft_strstr(new->key, special_char) == 0)
-		return (1);
 	new->value = ft_memmove(new->value, (ft_strchr(arg, '=') + 1), vlen);
 	new->value[vlen] = '\0';
-	new->next = NULL;
-	ft_env_lstadd_back(&env_lst, new);
+	return (new);
+}
+
+// 10.20 (Sun) bashで、export FOOなどの終了ステータスを見た場合、全部0だった
+// returnを全部0にしている
+int	export(char *arg, t_env *env_lst)
+{
+	t_env	*new;
+
+	// 引数が key=value の形になっていなかっらはじく
+	if (!ft_strchr(arg, '='))
+		return (0);
+
+	if (is_special_char_in_key(arg) == true)
+		return (0);
+	new = get_node_having_same_key(arg, env_lst);
+	if (new == NULL)
+	{
+		new = malloc(sizeof(t_env));
+		if (!new)
+			return (0);
+		new = set_key_value_registered(new, arg);
+		if (new == NULL)
+			return (free(new), new = NULL, 0);
+		new->next = NULL;
+		ft_env_lstadd_back(&env_lst, new);
+	}
+	else
+	{
+		printf("\x1b[36mThere is a node having a same key.\x1b[0m\n%s\n", new->key);
+		new = set_key_value_registered(new, arg);
+		if (new == NULL)
+			return (0);
+	}
 	return (0);
 }
 
-int    main(int argc, char *argv[], char *envp[])
-{
-	int	i;
-	t_env	*head;
-	t_env	*env_lst;
+// int    main(int argc, char *argv[], char *envp[])
+// {
+// 	int	i;
+// 	t_env	*head;
+// 	t_env	*env_lst;
 
-	env_lst = ft_env_lst(envp);
-	if (!env_lst)
-		return (1);
-	if (argc != 2)
-		return (0);
-	int status = export(argv[1], env_lst);
-	head = env_lst;
-	i = 0;
-	while (env_lst != NULL)
-	{
-		printf("%s=%s\n", env_lst->key, env_lst->value);
-		env_lst = env_lst->next;
-	}
-	ft_env_lstclear(&head, ft_free_env_node);
-	return (status);
-}
+// 	env_lst = ft_env_lst(envp);
+// 	if (!env_lst)
+// 		return (1);
+// 	if (argc != 2)
+// 		return (0);
+// 	int status = export(argv[1], env_lst);
+// 	head = env_lst;
+// 	i = 0;
+// 	while (env_lst != NULL)
+// 	{
+// 		printf("%s=%s\n", env_lst->key, env_lst->value);
+// 		env_lst = env_lst->next;
+// 	}
+// 	ft_env_lstclear(&head, ft_free_env_node);
+// 	return (status);
+// }
