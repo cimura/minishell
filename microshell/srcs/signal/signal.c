b64b0dd1516266cf2ead5288ec8858fc75ad5426
Takato Takino <ttakino@c2r2s4.42tokyo.jp>
2024-11-13 14:17:11 +0900

    ヘッダーを追加

--- microshell/srcs/signal/signal.c
+++ microshell/srcs/signal/signal.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttakino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 14:16:02 by ttakino           #+#    #+#             */
+/*   Updated: 2024/11/13 14:16:05 by ttakino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -22,36 +34,36 @@ void	ft_signal(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-//int main(int argc, char **argv, char **envp)
-//{
-//	(void)argv;
-//	(void)argc;
-//	(void)envp;
-//	char *line = NULL;
+int main(int argc, char **argv, char **envp)
+{
+	(void)argv;
+	(void)argc;
+	(void)envp;
+	char *line = NULL;
 
-//	ft_signal();
+	ft_signal();
 
-//	while (1)
-//	{
-//		line = readline("minishell> ");
+	while (1)
+	{
+		line = readline("minishell> ");
 
-//		if (line == NULL)
-//		{
-//		  printf("exit\n");
-//		  break;
-//		}
-//		if (ft_strlen(line) == 0)
-//		{
-//		  free(line);
-//		  continue;
-//		}
-//		else
-//		{
-//			printf("input: %s\n", line);
-//		}
-//		if (ft_strlen(line) > 0)
-//			add_history(line);
-//		free(line); // Free memory allocated by readline
-//	}
-//	return (0);
-//}
+		if (line == NULL)
+		{
+		  printf("exit\n");
+		  break;
+		}
+		if (ft_strlen(line) == 0)
+		{
+		  free(line);
+		  continue;
+		}
+		else
+		{
+			printf("input: %s\n", line);
+		}
+		if (ft_strlen(line) > 0)
+			add_history(line);
+		free(line); // Free memory allocated by readline
+	}
+	return (0);
+}
