070254063ea6915c99bf8a79ae2b1fb340406d0f
taka2162 <talat.talat@outlook.com>
2024-11-14 15:15:29 +0900

    mainをコメントアウト

--- microshell/srcs/signal/signal.c
+++ microshell/srcs/signal/signal.c
@@ -34,36 +34,36 @@ void	ft_signal(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-int main(int argc, char **argv, char **envp)
-{
-	(void)argv;
-	(void)argc;
-	(void)envp;
-	char *line = NULL;
+//int main(int argc, char **argv, char **envp)
+//{
+//	(void)argv;
+//	(void)argc;
+//	(void)envp;
+//	char *line = NULL;
 
-	ft_signal();
+//	ft_signal();
 
-	while (1)
-	{
-		line = readline("minishell> ");
+//	while (1)
+//	{
+//		line = readline("minishell> ");
 
-		if (line == NULL)
-		{
-		  printf("exit\n");
-		  break;
-		}
-		if (ft_strlen(line) == 0)
-		{
-		  free(line);
-		  continue;
-		}
-		else
-		{
-			printf("input: %s\n", line);
-		}
-		if (ft_strlen(line) > 0)
-			add_history(line);
-		free(line); // Free memory allocated by readline
-	}
-	return (0);
-}
+//		if (line == NULL)
+//		{
+//		  printf("exit\n");
+//		  break;
+//		}
+//		if (ft_strlen(line) == 0)
+//		{
+//		  free(line);
+//		  continue;
+//		}
+//		else
+//		{
+//			printf("input: %s\n", line);
+//		}
+//		if (ft_strlen(line) > 0)
+//			add_history(line);
+//		free(line); // Free memory allocated by readline
+//	}
+//	return (0);
+//}
